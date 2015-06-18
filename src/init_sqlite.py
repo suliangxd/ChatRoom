25da6845da104d574fe3dd6feb074ac0975599f5
SuLiang <suliangxd@gmail.com>
2015-06-18 14:41:05 +0800

    完成sqlite数据库 user表的建立代码

--- src/init_sqlite.py
+++ src/init_sqlite.py
@@ -1,1 +1,32 @@
-#coding
+#coding:utf-8
+
+import sqlite3
+
+con = sqlite3.connect('chatroom.db')
+cur = con.cursor()
+
+command = """
+BEGIN;
+CREATE TABLE IF NOT EXISTS user(
+	"userid" integer PRIMARY KEY AUTOINCREMENT,
+	"username" varchar(20) NOT NULL, 
+	"password" varchar(50) NOT NULL,
+	"registed_time" datetime NOT NULL,
+	"isvip" smallint DEFAULT 0,
+	"isadmin" smallint DEFAULT 0,
+	"email" varchar(30),
+	"phone" varchar(20),
+	UNIQUE("username") 
+);
+COMMIT;
+"""
+
+try:
+	cur.executescript(command)
+	con.commit()
+except Exception as e:
+	print e
+
+cur.close()
+con.close()
+
